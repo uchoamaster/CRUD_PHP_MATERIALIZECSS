1caa14606a468607c9e45886ad47511859030fec
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-08-16 13:11:04 -0300

    arquivos crud iniciados

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+    <nav class="light-blue accent-3">
+        <div class="nav-wrapper container">
+            <div class="brand-logo light">Sistema de Cadastro</div>
+            <ul class="right">
+                <li><a href=""><i class="material-icons left">account_circle</i> Cadastro</a></li>
+                <li><a href=""><i class="material-icons left">search</i> Consultas</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- formulário de cadastro -->
+    
+ 
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+
+      <!-- inicialização JQUERY -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        // alert("olá mundo");
+    });
+</script>
+    </body>
+  </html>
+        
